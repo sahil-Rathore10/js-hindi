679474ab2224c5020da5217b477bf8ccb3a15a27
Sahil Rathore <129116592+sahil-Rathore10@users.noreply.github.com>
2024-04-04 07:42:02 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = "33abc"  convert toh ho jayega per output NaN aata
+// let score= null ye b convert ho jayega but output 0 ayega
+// let score= undefined   ye b convert ho jayega but output NaN ayega
+// let score= true convert ho jayega but output 1 ayega
+// let score="sahil" output NaN ayega but convert ho jayega number me
+// let score = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+// let isLoggedIn = 1 ture ayega output convert ho kr
+// let isLoggedIn = "" false ayega answeer convert ho kr
+let isLoggedIn = "sahil"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//  1 => true ; 0 => false
+//  "" => false
+//  "sahil" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
+
+
