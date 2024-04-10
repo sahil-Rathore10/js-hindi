37c8d3140293399579260d785eb1d9ffebf9afcf
Sahil Rathore <129116592+sahil-Rathore10@users.noreply.github.com>
2024-04-10 09:59:42 +0000

    comparison basics

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1); issme ye wale me datatype check ni hote islye true ayega
+// console.log("02" > 1);
+
+// this type of comparison is not allow in programming
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strickly check the datatypes and then send compare it
+
+console.log("2" === 2);
